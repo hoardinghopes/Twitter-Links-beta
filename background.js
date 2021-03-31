afbdd17a3964d1b23e4e0b4d42a64749f3f8335a
James Knight <36128458+hoardinghopes@users.noreply.github.com>
2021-03-31 17:08:54 +0100

    Updating for Firefox API

--- background.js
+++ background.js
@@ -1,17 +1,31 @@
 function openSearch(tab) {
-  chrome.windows.getCurrent(function(currentWindow) {
-    TwitterLinksPopup = window.open('http://twitter.com/search?q=' + tab.url.split('?')[0] + '&TwitterLinks', 'TwitterLinks-' + tab.id, 'top=' + (currentWindow.top + (currentWindow.height - tab.height)) + ', left=' + (currentWindow.left + tab.width - 400) + ', width=400, height=' + tab.height);
+  browser.windows.getCurrent().then((currentWindow) => {
+    let createData = {
+      url:
+        "http://twitter.com/search?q=" +
+        tab.url.split("?")[0] +
+        "&TwitterLinks",
+      type: "popup",
+      height: tab.height,
+      width: 600,
+      top: currentWindow.top + currentWindow.height - tab.height,
+      left: currentWindow.left + tab.width - 600,
+      titlePreface: "Twitter links to this page: ",
+    };
+    browser.windows.create(createData);
   });
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  openSearch(tab)
+browser.contextMenus.create({
+  id: "twitter-links-to-page",
+  title: "Twitter links to this page\u2026",
+  contexts: ["all"],
 });
 
-chrome.contextMenus.create({
-  title: "Twitter links to this page\u2026"
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  openSearch(tab);
 });
 
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  openSearch(tab)
+browser.browserAction.onClicked.addListener(function (tab) {
+  openSearch(tab);
 });
